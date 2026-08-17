4a11927bb4d921b2623df9f264805bd2deee2396
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-08-16 21:36:14 -0700

    fix: guard Darwin feature-test definition

--- src/internal/project_provider_clang_ast.h
+++ src/internal/project_provider_clang_ast.h
@@ -1,7 +1,7 @@
 #ifndef CATALYST_EVO_INTERNAL_PROJECT_PROVIDER_CLANG_AST_H
 #define CATALYST_EVO_INTERNAL_PROJECT_PROVIDER_CLANG_AST_H
 
-#if defined(__APPLE__)
+#if defined(__APPLE__) && !defined(_DARWIN_C_SOURCE)
 #define _DARWIN_C_SOURCE
 #endif
 
