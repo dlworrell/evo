888f6a5fee469dcd35a42947174766897f370227
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-08-16 21:39:04 -0700

    fix: avoid undefined Darwin feature-test evaluation

--- src/internal/project_provider_clang_ast.h
+++ src/internal/project_provider_clang_ast.h
@@ -1,8 +1,10 @@
 #ifndef CATALYST_EVO_INTERNAL_PROJECT_PROVIDER_CLANG_AST_H
 #define CATALYST_EVO_INTERNAL_PROJECT_PROVIDER_CLANG_AST_H
 
-#if defined(__APPLE__) && !defined(_DARWIN_C_SOURCE)
-#define _DARWIN_C_SOURCE
+#if defined(__APPLE__)
+#ifndef _DARWIN_C_SOURCE
+#define _DARWIN_C_SOURCE 1
+#endif
 #endif
 
 #include "internal/project_provider_clang.h"
