d9747e71a7a755be9d57e647709a076ec6d67d96
Donovan Worrell <donovan.worrell@just-a-geek.com>
2026-08-16 21:20:09 -0700

    fix: expose Darwin no-follow flags only for AST provider

--- src/internal/project_provider_clang_ast.h
+++ src/internal/project_provider_clang_ast.h
@@ -1,6 +1,10 @@
 #ifndef CATALYST_EVO_INTERNAL_PROJECT_PROVIDER_CLANG_AST_H
 #define CATALYST_EVO_INTERNAL_PROJECT_PROVIDER_CLANG_AST_H
 
+#if defined(__APPLE__)
+#define _DARWIN_C_SOURCE
+#endif
+
 #include "internal/project_provider_clang.h"
 
 #endif
